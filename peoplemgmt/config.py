9f281de71970c4ca50226d97398a8009c3ac28d8
Max Mehl <max.mehl@deutschebahn.com>
2024-04-05 10:21:27 +0200

    read configured members

--- peoplemgmt/config.py
+++ peoplemgmt/config.py
@@ -1,17 +1,22 @@
 """Handling the private and organisation configuration"""
 
+from typing import Any
+
 import yaml
 
 # Global file with settings for the app, e.g. GitHub token
 APP_CONFIG_FILE = "config/app_config.yaml"
 
 
-def get_config(file: str, setting: str) -> str:
+def get_config(file: str, setting: str = "") -> Any:
     """Get the value for a specific config"""
     with open(file, encoding="UTF-8") as yamlfile:
-        config = yaml.safe_load(yamlfile)
+        config: dict = yaml.safe_load(yamlfile)
 
-    return config[setting]
+    if setting:
+        return config.get(setting)
+
+    return config
 
 
 def get_app_config(setting: str) -> str:
--- peoplemgmt/osrd.py
+++ peoplemgmt/osrd.py
@@ -6,6 +6,8 @@ from github import Github
 
 from .ghapi import get_github_token
 
+from .config import get_config
+
 ORG = "OpenRailAssociation"
 
 
@@ -14,22 +16,32 @@ class GHorg:
     """Dataclass holding GH organization data and functions"""
 
     org = None
-    members: list[str] = field(default_factory=list)
+    current_members: list[str] = field(default_factory=list)
+    configured_members: dict[str, dict] = field(default_factory=dict)
 
     def login(self, orgname):
         """Login to GH, gather org data"""
         self.org = Github(get_github_token()).get_organization(orgname)
 
-    def build_members(self):
+    def get_current_members(self):
         """Get all current members of the org"""
         for member in self.org.get_members():
-            self.members.append(member.login)
+            self.current_members.append(member.login)
+
+    def read_configured_members(self):
+        """Import configured members of the org"""
+        self.configured_members = get_config("config/openrailassociation.yaml", "members")
+
+        for member, v in self.configured_members.items():
+            print(member)
+            print(v)
 
 
 def main():
     """Main function"""
     org = GHorg()
     org.login(ORG)
-    org.build_members()
+    org.get_current_members()
+    org.read_configured_members()
 
-    print(org)
+    # print(org)

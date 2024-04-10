5407fb533de1b947da984ff6b669f549e8a99ba4
Max Mehl <max.mehl@deutschebahn.com>
2024-04-10 17:26:32 +0200

    handle splitted config of app, org, and teams

--- gh_team_mgr/_gh_api.py
+++ gh_team_mgr/_gh_api.py
@@ -2,18 +2,18 @@
 Functions for interacting with the GitHub API
 """
 
+import logging
 import os
 import sys
 
-from ._config import get_app_config
 
-
-def get_github_token() -> str:
+def get_github_token(token: str = "") -> str:
     """Get the GitHub token from config or environment, while environment overrides"""
     if "GITHUB_TOKEN" in os.environ and os.environ["GITHUB_TOKEN"]:
+        logging.debug("GitHub Token taken from environment variable GITHUB_TOKEN")
         token = os.environ["GITHUB_TOKEN"]
-    elif get_app_config("GITHUB_TOKEN"):
-        token = get_app_config("GITHUB_TOKEN")
+    elif token:
+        logging.debug("GitHub Token taken from app configuration file")
     else:
         sys.exit(
             "No token set for GitHub authentication! Set it in config/app_config.yaml "

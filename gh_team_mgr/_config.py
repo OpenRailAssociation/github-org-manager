3299c98e5180a690bf61ebd3b52f971b8db21000
Max Mehl <max.mehl@deutschebahn.com>
2024-04-10 17:26:32 +0200

    handle splitted config of app, org, and teams

--- gh_team_mgr/_config.py
+++ gh_team_mgr/_config.py
@@ -1,15 +1,75 @@
 """Handling the private and organisation configuration"""
 
+import logging
+import os
+import re
+import sys
 from typing import Any
 
 import yaml
 
-# Global file with settings for the app, e.g. GitHub token
-APP_CONFIG_FILE = "config/app_config.yaml"
+# Global files with settings for the app and org, e.g. GitHub token and org name
+ORG_CONFIG_FILE = r"org\.ya?ml"
+APP_CONFIG_FILE = r"app\.ya?ml"
+TEAM_CONFIG_DIR = "teams"
+TEAM_CONFIG_FILES = r".+\.ya?ml"
 
 
-def get_config(file: str, setting: str = "") -> Any:
-    """Get the value for a specific config"""
+def _find_matching_files(directory: str, pattern: str, only_one: bool = False) -> list[str]:
+    """
+    Get all files in a directory matching a regex pattern.
+
+    Args:
+    - directory: Path to the directory
+    - pattern: Regular expression pattern to match filenames
+    - only_one: Whether only the first match shall be returned.
+
+    Returns:
+    - List of filenames matching the pattern
+    """
+    matching_files: list[str] = []
+
+    # Validate directory existence
+    if not os.path.isdir(directory):
+        logging.error("'%s' is not a valid directory", directory)
+
+    else:
+        # Compile the regex pattern
+        regex_pattern = re.compile(pattern)
+
+        # Traverse the directory and find matching files
+        for file_name in os.listdir(directory):
+            if regex_pattern.match(file_name):
+                file_path = os.path.join(directory, file_name)
+                if os.path.isfile(file_path):
+                    matching_files.append(file_path)
+                else:
+                    logging.warning(
+                        "'%s' looks like a file we searched for, but it's not. "
+                        "Will not consider its contents",
+                        file_path,
+                    )
+
+        if only_one and len(matching_files) > 1:
+            matching_files = [matching_files[0]]
+            logging.warning(
+                "More than one configuration file for the pattern '%s' found. "
+                "Reducing to the first match as wished: %s",
+                pattern,
+                matching_files[0],
+            )
+
+    if not matching_files:
+        logging.error(
+            "No configuration file found for '%s', the program might not work as expected!"
+        )
+
+    return matching_files
+
+
+def _read_config_file(file: str, setting: str = "") -> Any:
+    """Read a YAML file, optionally returning just a certain key"""
+    logging.debug("Attempting to parse YAML file %s", file)
     with open(file, encoding="UTF-8") as yamlfile:
         config: dict = yaml.safe_load(yamlfile)
 
@@ -19,6 +79,33 @@ def get_config(file: str, setting: str = "") -> Any:
     return config
 
 
-def get_app_config(setting: str) -> str:
-    """Get the app's config"""
-    return get_config(APP_CONFIG_FILE, setting)
+def parse_config_files(path: str) -> tuple[dict, dict, dict]:
+    """Parse all relevant files in the configuration directory"""
+    # Find the relevant config files for app, org, and teams
+    cfg_app_files = _find_matching_files(path, APP_CONFIG_FILE, only_one=True)
+    cfg_org_files = _find_matching_files(path, ORG_CONFIG_FILE, only_one=True)
+    cfg_teams_files = _find_matching_files(os.path.join(path, TEAM_CONFIG_DIR), TEAM_CONFIG_FILES)
+
+    # Read and parse config files for app and org
+    cfg_app = _read_config_file(cfg_app_files[0])
+    cfg_org = _read_config_file(cfg_org_files[0])
+
+    # For the teams config files, we parse and combine them as there may be multiple
+    cfg_teams: dict[str, Any] = {}
+    # For this, merge the resulting dicts of the previously read files, and the current file
+    # Compare their keys (team names). They must not be defined multiple times!
+    for cfg_team_file in cfg_teams_files:
+        cfg = _read_config_file(cfg_team_file)
+        if overlap := set(cfg_teams.keys()) & set(cfg.keys()):
+            logging.critical(
+                "The config file '%s' contains keys that are also defined in "
+                "other config files. This is disallowed. Affected keys: %s",
+                cfg_team_file,
+                ", ".join(overlap),
+            )
+            sys.exit(1)
+        else:
+            # Merge the dicts into one
+            cfg_teams = cfg_teams | cfg
+
+    return cfg_org, cfg_app, cfg_teams
--- gh_team_mgr/configure_teams.py
+++ gh_team_mgr/configure_teams.py
@@ -2,18 +2,20 @@
 
 import argparse
 import logging
+import sys
 
 from . import __version__, configure_logger
+from ._config import parse_config_files
 from ._gh_org import GHorg
 
 parser = argparse.ArgumentParser(
     description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
 )
 parser.add_argument(
-    "-o",
-    "--organization",
+    "-c",
+    "--config",
     required=True,
-    help="Name of the GitHub organization you would like to handle",
+    help="Path to the directory in which the configuration of an GitHub organisation is located",
 )
 parser.add_argument("--debug", action="store_true", help="Get verbose logging output")
 parser.add_argument("--dry", action="store_true", help="Do not make any changes at GitHub")
@@ -32,7 +34,16 @@ def main():
         logging.info("Dry-run mode activated, will not make any changes at GitHub")
 
     org = GHorg()
-    org.login(args.organization)
+
+    # Parse configuration folder, and do sanity check
+    cfg_org, cfg_app, org.configured_teams = parse_config_files(args.config)
+    if not cfg_org.get("org_name"):
+        logging.critical(
+            "No GitHub organisation name configured in organisation settings. Cannot continue"
+        )
+        sys.exit(1)
+
+    org.login(cfg_org.get("org_name", ""), cfg_app.get("github_token", ""))
     org.create_missing_teams(dry=args.dry)
     org.sync_teams_members(dry=args.dry)
     org.get_members_without_team()
